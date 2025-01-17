9a8816087486ec2946f248bd30c289945c73200c
Anna Keener <annahkeener@gmail.com>
2025-01-17 13:48:34 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+from qgis.core import Qgs
+import processing
+from modules import merge_vector
+from qgis.utils import iface
---
+++ modules/merge_vector.py
@@ -0,0 +1,23 @@
+from overlayMaker.main import *
+
+activeLayers = iface.layerTreeView().selectedLayers()
+
+layersListHolder = [activeLayers]
+
+layersPaths = []
+
+layerNames = ''
+
+for layersList in layersListHolder:
+    for layer in layersList:
+        layersPaths.append(layer.source())
+        layerNames = layerNames + layer.name() + '_'
+
+print(layerNames)
+
+
+outputPath = 'C:/wfh/per1/overlays/wet/domed/Vectors/Inner+Outer/' + layerNames + '.shp'
+
+processing.run("native:mergevectorlayers", {'LAYERS':layersPaths,'CRS':None,'OUTPUT':outputPath})
+
+iface.addVectorLayer(outputPath, layerNames, "ogr")
